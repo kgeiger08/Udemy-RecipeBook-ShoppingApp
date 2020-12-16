9079cf175daaa097ce161a13f6c0bb31ad8eb741
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-12-16 17:25:29 -0600

    completed section-10

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,42 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg')
+        new Recipe(
+            'A Tasty Schnitzel', 
+            'A super-tasty Schnitzel!', 
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Delicious Burger', 
+            'A tasty and delicious burger!', 
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+                new Ingredient('Cheese', 1),
+                new Ingredient('Pickles', 8),
+                new Ingredient('Lettuce', 1),
+                new Ingredient('Ketchup', 1)
+            ])
       ];
+
+      constructor(private slService: ShoppingListService) {}
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 }
