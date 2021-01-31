0d9d422e7aaaa4a173c156a0234e079d77f837d0
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2021-01-31 17:34:53 -0600

    moved error logic to the auth service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -32,9 +32,9 @@ export class AuthComponent {
             this.authService.signup(email, password).subscribe(resData => {
                 console.log(resData);
                 this.isLoading = false;
-            }, error => {
-                console.log(error);
-                this.error = 'An error occurred!'
+            }, errorMessage => {
+                console.log(errorMessage);
+                this.error = errorMessage;
                 this.isLoading = false;
             });
         }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
     idToken: string;
@@ -21,6 +23,16 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        );
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+            }
+            return throwError(errorMessage);
+        }));
     }
 }
