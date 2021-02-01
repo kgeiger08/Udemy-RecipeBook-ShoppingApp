cbf8482abf5c6f3ca61b1cadee6d7ad25ec5f212
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2021-02-01 00:25:16 -0600

    added login feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -23,16 +24,35 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred!';
+        ).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA3SKPm_3csztJikwKEWYBPxsKiF9DVEI8', 
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!';
             if (!errorRes.error || !errorRes.error.error) {
                 return throwError(errorMessage);
             }
             switch (errorRes.error.error.message) {
                 case 'EMAIL_EXISTS':
                     errorMessage = 'This email exists already';
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'The email or password does not exist.';
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'The email or password does not exist.';
+                    break;
             }
             return throwError(errorMessage);
-        }));
     }
 }
